b9eff23058cc037e3ae6ef8e753530160225a7a5
Takashi Sato <takashi@tks.st>
2023-01-22 00:05:36 +0900

    template to package

--- tsup.config.bin.mjs
+++
@@ -1,9 +0,0 @@
-/* eslint import/no-extraneous-dependencies: 0 */
-
-import { binOptions } from "@tksst/typescript-starter-configs/tsup-config.mjs";
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-    ...binOptions,
-    entry: ["src/bin/index.ts"],
-});
